0869f79c6893e7ab9bad0100b1d2df5dcc908037
Andrew TOwnsley <atownsley86@gmail.com>
2022-05-30 11:23:52 -0600

    Dayjs library formatting used.

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 const dateDisplay = document.getElementById('dateDisplay');
 
+////////////////////////////// Day.js Date Library
+const dayJsObj = dayjs()
+dateDisplay.innerHTML = dayJsObj.format(("M/D/YYYY h:mm A"));
+console.log(dayJsObj.format(("M/D/YYYY h:mm A")));
 
-const myDate = new Date();
-const myDateFormatted = formatDate(myDate);
-// dateDisplay.innerHTML = myDate.toDateString();
-dateDisplay.innerHTML = myDateFormatted;
-
-
-function formatDate(dateObject) {
-    // console.log("Format Date Function:");
-    const parts = {
-        month: (dateObject.getMonth() + 1),
-        date: dateObject.getDate(),
-        year: dateObject.getFullYear(),
-        hour: (dateObject.getHours() % 12) || 12,
-        minute: dateObject.getMinutes(),
-        amPm: dateObject.getHours() < 12 ? "am" : "pm"
-    }
-    // console.log("parts: ", parts);
-    return `
-         ${parts.month} / ${parts.date} / ${parts.year}, 
-         ${parts.hour}:${parts.minute} ${parts.amPm}
-    `;
-}
+//////////////////// Vanilla Javascript Date Object...
+// const myDate = new Date();
+// const myDateFormatted = formatDate(myDate);
+// // dateDisplay.innerHTML = myDate.toDateString();
+// dateDisplay.innerHTML = myDateFormatted;
 
 
-console.log("myDateFormatted: ", myDateFormatted);
+// function formatDate(dateObject) {
+//     // console.log("Format Date Function:");
+//     const parts = {
+//         month: (dateObject.getMonth() + 1),
+//         date: dateObject.getDate(),
+//         year: dateObject.getFullYear(),
+//         hour: (dateObject.getHours() % 12) || 12,
+//         minute: dateObject.getMinutes().toString().padStart(2, "0"),
+//         amPm: dateObject.getHours < 12 ? "am" : "pm"
+//     }
+//     // console.log("parts: ", parts);
+//     return `
+//          ${parts.month} / ${parts.date} / ${parts.year}, 
+//          ${parts.hour}:${parts.minute} ${parts.amPm}
+//     `;
+// }
+// console.log("myDateFormatted: ", myDateFormatted);
+/////////////////////////////////////////////////////
